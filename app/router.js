2b6bd9ef0a9bf3e83259ff832c977115d7647334
Maxime SIKORA <siko59176@hotmail.fr>
2023-06-13 15:30:33 +0200

    ajustement

--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// require controllers
 
+// require controllers
+const mainController = require('./controller/mainController')
 
 // Les routes
-
+router.get('/', mainController.index)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -8,33 +8,32 @@ const session = require('express-session');
 const path = require('path');
 
 // Local imports
-const router = require('./app/routers');
+const router = require('./app/router');
 
-// Body parser
+// Body parser pour utilisation de route POST
 app.use(express.urlencoded({ extended: true }));
 
-// Charger les données de la sessions sur `req.session` et `res.locals`
-app.use(
-    session({
-        saveUninitialized: true,
-        resave: true,
-        secret: 'Un secret pour signer les id de sessions',
-    })
-);
-app.use(loadUserToLocals);
+// ! Si utilisation de session
+// app.use(
+//     session({
+//         saveUninitialized: true,
+//         resave: true,
+//         secret: 'Un secret pour signer les id de sessions',
+//     })
+// );
 
 // Setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-// Statically served files
+// Statically served files (css, JS front, img, favicon.ico)
 app.use(express.static(path.join(__dirname, './public')));
 
 // Nos Routes
 app.use(router);
 
 // middleware 404 à configurer
-app.use();
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
